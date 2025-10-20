9f6d1193f5446d84f62b8ebb8d8f30ba4418fef9
dev-Arsl-an <arslanlatif067@gmail.com>
2025-10-21 03:13:17 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World! I am running Flask. '
+
+@app.route('/page')
+def page():
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.run(debug=True)
